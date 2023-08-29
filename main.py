ca53b74ee9e1e50e0aa5778e4485febf6c3a9e88
JPersonalCode <143402072+JPersonalCode@users.noreply.github.com>
2023-08-29 13:28:19 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# Globals
+player_range_select = ""
+playyer_guess = ""
+# Functions 
+def validate_input(input):
+  try :
+    int(input)
+  else:
+    raise Exception("not valid input")
 
+# Call Stack
+
+while True : 
+  player_range_select = input("Please the number at the top of the range you would like? " )
+  player_guess = input("Please Place your Guess: ")
+  validate_input(player_range_select)
+  validate_input(player_guess)
